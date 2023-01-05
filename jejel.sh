b68f9d7968b1c7c788f40c5fac0c04a2f4ccdaae
antinsp <66296176+antinsp@users.noreply.github.com>
2023-01-06 00:13:31 +0700

    Update jejel.sh

--- jejel.sh
+++ jejel.sh
@@ -12,9 +12,9 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 
-MYIP='103.191.92.89';
+MYIP='103.23.199.61';
 clear
-domain='go.kereaktif.tk'
+domain='mod.kereaktif.tk'
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
