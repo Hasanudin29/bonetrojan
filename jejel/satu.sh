e018bf91ae07ce4d99d98b74f03af24a1de8ee57
Hasanudin29 <135009835+Hasanudin29@users.noreply.github.com>
2023-06-19 09:38:21 +0700

    Update satu.sh

--- jejel/satu.sh
+++ jejel/satu.sh
@@ -194,17 +194,17 @@ http {
 #}
 END
 
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/ak
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/del
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/menu
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/renew
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/restart
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/xp
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/xp_cron
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/reboot_vps
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/reboot_otomatis.sh
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/certv2ray
-wget https://raw.github.com/antinsp/bonetrojan/main/jejel/menu/addhost
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/ak
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/del
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/menu
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/renew
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/restart
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/xp
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/xp_cron
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/reboot_vps
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/reboot_otomatis.sh
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/certv2ray
+wget https://raw.github.com/Hasanudin29/bonetrojan/main/jejel/menu/addhost
 
 mv certv2ray /bin/certv2ray
 mv addhost /bin/addhost
